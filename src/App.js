a8d27913e5699f75eee690b8aa4dfec9ec4a2388
tungmk97 <tungnv975@gmail.com>
2022-07-11 17:39:00 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { debounce } from 'throttle-debounce';
+import * as BooksAPI from './api/BooksAPI';
 import './App.css';
+import ListBooks from './components/ListBooks';
+import SearchBooks from './components/SearchBooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const bookshelves = [
+  { key: 'currentlyReading', name: 'Currently Reading' },
+  { key: 'wantToRead', name: 'Want to Read' },
+  { key: 'read', name: 'Read' },
+];
+
+class App extends Component {
+  state = {
+    listBook: [],
+    listBookSearch: [],
+  };
+
+  componentDidMount = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ listBook: books });
+    });
+    console.log(this.state.listBook);
+  };
+
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).catch((err) => {
+      console.log(err);
+    });
+    if (shelf === 'none') {
+      this.setState((oldState) => ({
+        listBook: oldState.listBook.filter((b) => b.id !== book.id),
+      }));
+    } else {
+      book.shelf = shelf;
+      this.setState((oldState) => ({
+        listBook: oldState.listBook
+          .filter((b) => b.id !== book.id)
+          .concat(book),
+      }));
+    }
+  };
+
+  // debounce - up performance
+  searchForBooks = debounce(200, false, (searchParam) => {
+    if (searchParam.length > 0) {
+      BooksAPI.search(searchParam).then((books) => {
+        if (books.error) {
+          this.setState({ listBookSearch: [] });
+        } else {
+          this.setState({ listBookSearch: books });
+        }
+      });
+    } else {
+      this.setState({ listBookSearch: [] });
+    }
+  });
+
+  resetSearch = () => {
+    this.setState({ listBookSearch: [] });
+  };
+
+  render() {
+    const { listBook, listBookSearch } = this.state;
+    console.log(this.state);
+
+    if (!listBook.length > 0) {
+      return <div>Cannot fetch books</div>;
+    }
+    return (
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListBooks
+              shelves={bookshelves}
+              books={listBook}
+              onChangeShelf={this.changeShelf}
+            />
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBooks
+              listBookSearch={listBookSearch}
+              listBook={listBook}
+              onSearch={this.searchForBooks}
+              onChangeShelf={this.changeShelf}
+              onResetSearch={this.resetSearch}
+            />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ListBooks.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Shelf from './Shelf';
+import { Link } from 'react-router-dom';
+
+
+class ListBooks extends Component {
+  render() {
+    const { shelves, books, onChangeShelf } = this.props;
+    console.log('ListBook');
+    console.log(shelves);
+    console.log(books);
+
+    return (
+      <div className="list-books">
+        <div className="books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="books-content">
+          <div>
+            {shelves.map((shelf) =>
+              shelf ? (
+                <Shelf
+                  key={shelf.key}
+                  shelf={shelf}
+                  books={books}
+                  onChangeShelf={onChangeShelf}
+                />
+              ) : (
+                'No Book'
+              )
+            )}
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="search">
+            <button>Search</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListBooks;
